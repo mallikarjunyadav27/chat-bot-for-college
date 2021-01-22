d126a48eba2d2d954fb4288b35b28c45ec7d653f
mallikarjun yadav <48082071+mallikarjunyadav27@users.noreply.github.com>
2021-01-22 21:24:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# chat-bot-for-college
-Chat-bot-for-college-
+# Chat-bot-for-college-
+Chat bot for college using NLP, Pytorch, Feed forward neural network
+Chatbots are conversational interfaces that can integrate artificial intelligence with different use cases. They are currently quite popular and organizations of all kinds are giving completely innovative uses to this technology.Little by little, chatbots have been entering education, their job so far is not to replace the figure of the teacher, but rather to assume repetitive tasks and of low cognitive level. This is how bots can improve productivity by answering frequently asked questions and as helpers or tutors that accompany the learning process.
+Specifically, in the educational environment we find chatbots of two types. First, there are those who do not have an educational intent and work to favor management processes and as personal assistance to students. And then there are those who have a clear educational intent and function as tutors that accompany the student in their learning process or create a more concrete exercise and practice environment
+
+Among the many use cases Frequently Asked Question is a main concern that institutions face. 
+Here we choose to concentrate on building a chatbot for Frequently Asked Question in favour of our College’s administration to optimize the same
---
+++ chat/pytorch-chatbot-master/app.py.py
@@ -0,0 +1,61 @@
+from flask import Flask,render_template,request
+import random
+import json
+import torch
+
+from model  import NeuralNet
+from nltk1 import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+my=open('C:/Users/sai kiran Reddy/Downloads/malli intents.json','r')
+jsondata=my.read()
+intents=json.loads(jsondata)
+print(intents)
+
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+app = Flask(__name__)
+
+bot_name = "Sam"
+print("Let's chat! (type 'quit' to exit)")
+while True:
+    # sentence = "do you use credit cards?"
+    sentence = input("You: ")
+    if sentence == "Thankyou":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                reply=random.choice(intent['responses'])
+                print(f"{bot_name}",reply)
+    else:
+        print(f"{bot_name}: I do not understand...")
+
+
+            
